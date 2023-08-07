4e715b88f5fe9f6f2d62d7be0c9256face8877e0
Sanjay M R <sanjayilaya2002@gmail.com>
2023-08-07 20:40:14 +0530

    uploading day4 exercise 3 / Treasure X

---
+++ main (25).py
@@ -0,0 +1,20 @@
+
+import random
+row1 = ["⬜️️","⬜️️","⬜️️"]
+row2 = ["⬜️️","⬜️️","⬜️️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+#Write your code below this row 👇
+horizontal = int(position[1])
+vertical = int(position[0])
+selected_row = map[vertical-1] 
+selected_row[horizontal-1] = "X"
+
+
+
+print(f"{row1}\n{row2}\n{row3}")
+
